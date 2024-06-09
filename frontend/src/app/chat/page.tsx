9a650fccda4263bf6f7272bea96559c78e24fc84
Lyubomir Ternavskiy <127299159+LyubomirT@users.noreply.github.com>
2024-06-09 05:55:14 -0400

    Update page.tsx

--- frontend/src/app/chat/page.tsx
+++ frontend/src/app/chat/page.tsx
@@ -7,244 +7,250 @@ import { IoSendSharp } from "react-icons/io5";
 import io, { Socket } from "socket.io-client";
 
 interface Message {
-    name: string;
-    text: string;
-    date: string;
+	name: string;
+	text: string;
+	date: string;
 }
 
 export default function ChatPage() {
-    const [message, setMessage] = useState("");
-    const [messages, setMessages] = useState<Message[]>([]);
-    const [name, setName] = useState("");
-    const [typing, setTyping] = useState(false);
-    const [whoIsTyping, setWhoIsTyping] = useState("");
-    const [count, setCount] = useState(0);
-    const [clientsTotal, setClientsTotal] = useState(0);
+	const [message, setMessage] = useState("");
+	const [messages, setMessages] = useState<Message[]>([]);
 
-    const router = useRouter();
-    const socketRef = useRef<Socket | null>(null);
-    const messagesEndRef = useRef<null | HTMLDivElement>(null);
+	const [name, setName] = useState("");
 
-    const sendMessage = (event: React.FormEvent<HTMLFormElement> | KeyboardEvent) => {
-        if (event instanceof KeyboardEvent) {
-            if (event.key === 'Enter' && !event.shiftKey) {
-                event.preventDefault();
-            } else {
-                return;
-            }
-        } else {
-            event.preventDefault();
-        }
+	const [typing, setTyping] = useState(false);
+	const [whoIsTyping, setWhoIsTyping] = useState("");
 
-        if (!message.trim()) {
-            alert("You can't send an empty message.");
-            return;
-        }
+	const [count, setCount] = useState(0);
 
-        if (message.length > 1000) {
-            alert("You can't send a message with more than 1000 characters.");
-            return;
-        }
+	const [clientsTotal, setClientsTotal] = useState(0);
 
-        if (socketRef.current) {
-            socketRef.current.emit(
-                "createMessage",
-                {
-                    name,
-                    text: message,
-                    date: new Date()
-                        .toLocaleTimeString("en-US", {
-                            timeStyle: "short",
-                        })
-                        .toString(),
-                },
-                (response: Message) => {
-                    setMessage("");
-                }
-            );
-        }
-    };
+	const router = useRouter();
 
-    const join = useCallback((value: string) => {
-        (() => {
-            if (socketRef.current) {
-                socketRef.current.emit("join", { name: value });
-            }
-        })();
-    }, []);
+	const socketRef = useRef<Socket | null>(null);
 
-    const emitTyping = () => {
-        const socketExist: Socket | null = socketRef.current;
-        if (socketExist) {
-            socketExist.emit("typing", { isTyping: true });
-            setTimeout(() => {
-                if (socketExist) {
-                    socketExist.emit("typing", { isTyping: false });
-                }
-            }, 2000);
-        }
-    };
+	const messagesEndRef = useRef<null | HTMLDivElement>(null);
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        const textarea = event.target;
-        textarea.style.height = "auto";
-        textarea.style.height = textarea.scrollHeight + "px";
-        setMessage(event.target.value);
-        setCount(event.target.value.length);
-        emitTyping();
-    };
+	const sendMessage = (event: React.FormEvent<HTMLFormElement> | KeyboardEvent) => {
+		if (event instanceof KeyboardEvent) {
+			if (event.key === 'Enter' && !event.shiftKey) {
+				event.preventDefault();
+			} else {
+				return;
+			}
+		} else {
+			event.preventDefault();
+		}
 
-    const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-        if (event.key === 'Enter' && !event.shiftKey) {
-            sendMessage(event);
-        }
-    };
+		if (!message.trim()) {
+			alert("You can't send an empty message.");
+			return;
+		}
 
-    // This useEffect handles the user's name
-    useEffect(() => {
-        const nameGet = localStorage.getItem("name");
+		if (message.length > 1000) {
+			alert("You can't send a message with more than 1000 characters.");
+			return;
+		}
 
-        if (!nameGet || nameGet === "Bot") {
-            if (nameGet === "Bot") {
-                localStorage.removeItem("name");
-                alert("You can't use the name 'Bot'");
-            }
-            router.push("/");
-        } else {
-            setName(nameGet);
-            join(nameGet);
-        }
-    }, [join, router]);
+		if (socketRef.current) {
+			socketRef.current.emit(
+				"createMessage",
+				{
+					name,
+					text: message,
+					date: new Date()
+						.toLocaleTimeString("en-US", {
+							timeStyle: "short",
+						})
+						.toString(),
+				},
+				(response: Message) => {
+					setMessage("");
+				},
+			);
+		}
+	};
 
-    // This useEffect handles the socket connection
-    useEffect(() => {
-        socketRef.current = io(
-            process.env.NEXT_PUBLIC_DNS_SERVER_CONNECTION_URL
-                ? process.env.NEXT_PUBLIC_DNS_SERVER_CONNECTION_URL
-                : "https://chat-server-uxdw.onrender.com"
-        );
+	const join = useCallback((value: string) => {
+		(() => {
+			if (socketRef.current) {
+				socketRef.current.emit("join", { name: value });
+			}
+		})();
+	}, []);
 
-        socketRef.current.on("clientsTotal", (total: number) => {
-            setClientsTotal(total);
-        });
+	const emitTyping = () => {
+		const socketExist: Socket | null = socketRef.current;
+		if (socketExist) {
+			socketExist.emit("typing", { isTyping: true });
+			setTimeout(() => {
+				if (socketExist) {
+					socketExist.emit("typing", { isTyping: false });
+				}
+			}, 2000);
+		}
+	};
 
-        return () => {
-            if (socketRef.current) {
-                socketRef.current.close();
-            }
-        };
-    }, []);
+	const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+		const textarea = event.target;
+		textarea.style.height = "auto";
+		textarea.style.height = textarea.scrollHeight + "px";
+		setMessage(event.target.value);
+		setCount(event.target.value.length);
+		emitTyping();
+	};
 
-    // This useEffect handles the scroll to the bottom of the chat
-    useEffect(() => {
-        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-    }, [messages]);
+	const handleKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
+		if (event.key === 'Enter' && !event.shiftKey) {
+			sendMessage(event);
+		}
+	};
 
-    // This useEffect handles the messages and typing events
-    useEffect(() => {
-        const handleTyping = (data: { name: string; isTyping: boolean }) => {
-            if (data.name !== name) {
-                // Ignore typing events from the current user
-                setTyping(data.isTyping);
+	// This useEffect handles the user's name
+	useEffect(() => {
+		const nameGet = localStorage.getItem("name");
 
-                if (data.isTyping) {
-                    setWhoIsTyping(data.name);
-                } else {
-                    setWhoIsTyping("");
-                }
-            }
-        };
+		if (!nameGet || nameGet === "Bot") {
+			if (nameGet === "Bot") {
+				localStorage.removeItem("name");
+				alert("You can't use the name 'Bot'");
+			}
+			router.push("/");
+		} else {
+			setName(nameGet);
+			join(nameGet);
+		}
+	}, [join, router]);
 
-        const handleNewMessage = (message: Message) => {
-            setMessages((prev) => [...prev, message]);
-        };
+	// This useEffect handles the socket connection
+	useEffect(() => {
+		socketRef.current = io(
+			process.env.NEXT_PUBLIC_DNS_SERVER_CONNECTION_URL
+				? process.env.NEXT_PUBLIC_DNS_SERVER_CONNECTION_URL
+				: "https://chat-server-uxdw.onrender.com",
+		);
 
-        if (socketRef.current) {
-            socketRef.current.on("typing", handleTyping);
+		socketRef.current.on("clientsTotal", (total: number) => {
+			setClientsTotal(total);
+		});
 
-            socketRef.current.emit("findAllMessages", {}, (response: Message[]) => {
-                setMessages(response);
-            });
+		return () => {
+			if (socketRef.current) {
+				socketRef.current.close();
+			}
+		};
+	}, []);
 
-            socketRef.current.on("newMessage", handleNewMessage);
-        }
+	// This useEffect handles the scroll to the bottom of the chat
+	useEffect(() => {
+		messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+	}, [messages]);
 
-        // Limpeza ao desmontar
-        return () => {
-            if (socketRef.current) {
-                socketRef.current.off("newMessage", handleNewMessage);
-                socketRef.current.off("typing", handleTyping);
-            }
-        };
-    }, [name]);
+	// This useEffect handles the messages and typing events
+	useEffect(() => {
+		const handleTyping = (data: { name: string; isTyping: boolean }) => {
+			if (data.name !== name) {
+				// Ignore typing events from the current user
+				setTyping(data.isTyping);
 
-    return (
-        <div className="flex flex-col h-screen bg-gray-100">
-            <div className="flex items-center flex-col sm:flex-row justify-evenly py-2 bg-blue-500 text-white">
-                <div className="flex items-center justify-center gap-1">
-                    <FaCircle color={clientsTotal > 0 ? "green" : "gray"} size={22} />
-                    <span className="text-white">{clientsTotal}</span> users online
-                </div>
-                <h1 className="text-lg font-bold text-white mt-2 sm:mt-0">Chat</h1>
-                <p className="text-white p-2">Chat will be cleaned every 15 minutes.</p>
-            </div>
-            <div className="flex-1 flex flex-col overflow-y-auto p-2 space-y-2">
-                {messages.map((msg, index) => {
-                    return (
-                        <div
-                            key={index}
-                            className={`${
-                                msg.name === (localStorage.getItem("name") as string)
-                                    ? "self-end bg-green-500 text-white rounded-l-lg rounded-tr-lg"
-                                    : "self-start bg-white text-black rounded-r-lg rounded-tl-lg"
-                            } p-2 max-w-xs min-w-24 font-medium`}
-                        >
-                            {msg.name}:{" "}
-                            <span style={{ wordWrap: "break-word" }}>{msg.text}</span>
-                            <span className="inline-block opacity-65 ml-2 relative top-2 text-sm">
-                                {msg.date
-                                    ? msg.date
-                                    : new Date().toLocaleTimeString("en-US", {
-                                            timeStyle: "short",
-                                        })}
-                            </span>
-                        </div>
-                    );
-                })}
-                <div ref={messagesEndRef} />
-            </div>
-            <form
-                onSubmit={sendMessage}
-                action=""
-                className="flex flex-col p-2 bg-white"
-            >
-                <div className="flex justify-between mb-2">
-                    <p className="text-black mr-4">
-                        <span className={`${count > 999 ? "text-red-500" : "text-black"}`}>
-                            {count > 999 ? "Max" : count}
-                        </span>{" "}
-                        / 1000 characters.
-                    </p>
-                    <span className="text-black ml-4">
-                        {typing ? `${whoIsTyping} is typing...` : ""}
-                    </span>
-                </div>
+				if (data.isTyping) {
+					setWhoIsTyping(data.name);
+				} else {
+					setWhoIsTyping("");
+				}
+			}
+		};
 
-                <div className="flex">
-                    <textarea
-                        value={message}
-                        maxLength={1000}
-                        onChange={handleInputChange}
-                        onKeyDown={handleKeyDown}
-                        className="flex-grow p-2 border rounded-lg min-h-16 text-black max-h-28 overflow-auto resize-none"
-                        placeholder="Type a message..."
-                    />
+		const handleNewMessage = (message: Message) => {
+			setMessages((prev) => [...prev, message]);
+		};
 
-                    <button className="ml-2 px-4 py-2 bg-green-500 text-white rounded-lg active:bg-green-700 transition duration-150 ease-in-out">
-                        <IoSendSharp color="white" size={32} />
-                    </button>
-                </div>
-            </form>
-        </div>
-    );
+		if (socketRef.current) {
+			socketRef.current.on("typing", handleTyping);
+
+			socketRef.current.emit("findAllMessages", {}, (response: Message[]) => {
+				setMessages(response);
+			});
+
+			socketRef.current.on("newMessage", handleNewMessage);
+		}
+
+		// Limpeza ao desmontar
+		return () => {
+			if (socketRef.current) {
+				socketRef.current.off("newMessage", handleNewMessage);
+				socketRef.current.off("typing", handleTyping);
+			}
+		};
+	}, [name]);
+
+	return (
+		<div className="flex flex-col h-screen bg-gray-100">
+			<div className="flex items-center flex-col sm:flex-row justify-evenly py-2 bg-blue-500 text-white">
+				<div className="flex items-center justify-center gap-1">
+					<FaCircle color={clientsTotal > 0 ? "green" : "gray"} size={22} />
+					<span className="text-white">{clientsTotal}</span> users online
+				</div>
+				<h1 className="text-lg font-bold text-white mt-2 sm:mt-0">Chat</h1>
+				<p className="text-white p-2">Chat will be cleaned every 15 minutes.</p>
+			</div>
+			<div className="flex-1 flex flex-col overflow-y-auto p-2 space-y-2">
+				{messages.map((msg, index) => {
+					return (
+						<div
+							key={index}
+							className={`${
+								msg.name === (localStorage.getItem("name") as string)
+									? "self-end bg-green-500 text-white rounded-l-lg rounded-tr-lg"
+									: "self-start bg-white text-black rounded-r-lg rounded-tl-lg"
+							} p-2 max-w-xs min-w-24 font-medium`}
+						>
+							{msg.name}:{" "}
+							<span style={{ wordWrap: "break-word" }}>{msg.text}</span>
+							<span className="inline-block opacity-65 ml-2 relative top-2 text-sm">
+								{msg.date
+									? msg.date
+									: new Date().toLocaleTimeString("en-US", {
+											timeStyle: "short",
+										})}
+							</span>
+						</div>
+					);
+				})}
+				<div ref={messagesEndRef} />
+			</div>
+			<form
+				onSubmit={sendMessage}
+				action=""
+				className="flex flex-col p-2 bg-white"
+			>
+				<div className="flex justify-between mb-2">
+					<p className="text-black mr-4">
+						<span className={`${count > 999 ? "text-red-500" : "text-black"}`}>
+							{count > 999 ? "Max" : count}
+						</span>{" "}
+						/ 1000 characters.
+					</p>
+					<span className="text-black ml-4">
+						{typing ? `${whoIsTyping} is typing...` : ""}
+					</span>
+				</div>
+
+				<div className="flex">
+					<textarea
+						value={message}
+						maxLength={1000}
+						onChange={handleInputChange}
+						onKeyDown={handleKeyDown}
+						className="flex-grow p-2 border rounded-lg min-h-16 text-black max-h-28 overflow-auto resize-none"
+						placeholder="Type a message..."
+					/>
+
+					<button className="ml-2 px-4 py-2 bg-green-500 text-white rounded-lg active:bg-green-700 transition duration-150 ease-in-out">
+						<IoSendSharp color="white" size={32} />
+					</button>
+				</div>
+			</form>
+		</div>
+	);
 }
