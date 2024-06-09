bea79b9e3f037baec5999f981be3ee701dbdc962
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-06-09 11:02:59 -0300

    fix: message width overflow

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
 				"gradient-conic":
 					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
 			},
+			maxWidth: {
+				"80%": "80%",
+				"90%": "90%",
+			},
 		},
 	},
 	plugins: [],
