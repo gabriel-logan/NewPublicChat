278bb6a045f203739d9d5c46bea051ffbcd8ee20
gabriel-logan <gabrielcoutolima@protonmail.ch>
2024-06-12 12:04:43 -0300

    chore: Add react-toastify for displaying toast notifications

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+
 import "./globals.css";
+import "react-toastify/dist/ReactToastify.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +21,10 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				<ToastContainer autoClose={1500} />
+				{children}
+			</body>
 		</html>
 	);
 }
